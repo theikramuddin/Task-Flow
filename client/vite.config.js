fb70c186087859af973c4040e3bade46236c1f0e
IKRAMUDDIN <contact.ikramuddin@gmail.com>
2026-05-06 01:45:39 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['tasks-manager.up.railway.app'],
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
